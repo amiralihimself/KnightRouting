7ee1e420e3c97fac97b0ac41564c4ead19cd15ab
Amirali <madaniamirali7@gmail.com>
2024-03-13 17:59:50 -0400

    new classes added

---
+++ scripts/chessboard.py
@@ -0,0 +1,9 @@
+import graphics
+
+class chessboard:
+    def __init__(self, dimension, screenHeight, screenWidth):
+        self.dimension=dimension
+        self.screenwidth=screenWidth
+        self.screenheight=screenHeight
+
+
---
+++ scripts/main.py
@@ -0,0 +1,3 @@
+from chessboard import chessboard
+if __name__ == "__main__":
+     c1=chessboard(20,800,600)
