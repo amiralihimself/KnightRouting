a05287dcc6da148a9c5ee3440af47243de185c71
Amirali <madaniamirali7@gmail.com>
2024-03-13 18:45:20 -0400

    chessboard.py updated

--- scripts/chessboard.py
+++ scripts/chessboard.py
@@ -2,30 +2,34 @@ import graphics
 import numpy as np
 import time
 class chessboard:
-    def __init__(self, dimension, screenHeight, screenWidth):
+    def __init__(self, dimension, squareSize, screenHeight, screenWidth):
         self.dimension=dimension
         self.screenwidth=screenWidth
         self.screenheight=screenHeight
         self.boardArray=np.zeros((self.dimension, self.dimension))
+        self.squareSize= squareSize
 
     def drawBoard(self, screenTitle):
         window = graphics.GraphWin(screenTitle, self.screenwidth, self.screenheight)
         window.setBackground('lightblue')  # initializing the window's background
+        x=0
+        y=0
+        direction=True
         for j in range(self.dimension):
             for i in range(self.dimension):
                 pt1 = graphics.Point(x, y)
-                pt2 = graphics.Point(x + size, y + size)
-                rect = Rectangle(pt1, pt2)
+                pt2 = graphics.Point(x + self.squareSize, y + self.squareSize)
+                rect = graphics.Rectangle(pt1, pt2)
                 if direction:
                     rect.setFill("black")
                     direction = False  # making sure that two squares next to each other have different colors
                 else:
                     rect.setFill("white")
                     direction = True
-                rect.draw(win)
-                x += size
+                rect.draw(window)
+                x += self.squareSize
             direction = not direction
-            x = x_start
-            y += size
+            x = 0
+            y += self.squareSize
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,4 @@
 from chessboard import chessboard
 if __name__ == "__main__":
-     c1=chessboard(20,800,600)
+     c1=chessboard(20,30, 800,600)
      c1.drawBoard("salam")
