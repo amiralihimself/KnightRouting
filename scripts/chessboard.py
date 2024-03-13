79027b745b5b67e82481ce26299a3851f5339b4d
Amirali <madaniamirali7@gmail.com>
2024-03-13 18:35:09 -0400

    chessboard.py updated

--- scripts/chessboard.py
+++ scripts/chessboard.py
@@ -1,9 +1,31 @@
 import graphics
-
+import numpy as np
+import time
 class chessboard:
     def __init__(self, dimension, screenHeight, screenWidth):
         self.dimension=dimension
         self.screenwidth=screenWidth
         self.screenheight=screenHeight
+        self.boardArray=np.zeros((self.dimension, self.dimension))
+
+    def drawBoard(self, screenTitle):
+        window = graphics.GraphWin(screenTitle, self.screenwidth, self.screenheight)
+        window.setBackground('lightblue')  # initializing the window's background
+        for j in range(self.dimension):
+            for i in range(self.dimension):
+                pt1 = graphics.Point(x, y)
+                pt2 = graphics.Point(x + size, y + size)
+                rect = Rectangle(pt1, pt2)
+                if direction:
+                    rect.setFill("black")
+                    direction = False  # making sure that two squares next to each other have different colors
+                else:
+                    rect.setFill("white")
+                    direction = True
+                rect.draw(win)
+                x += size
+            direction = not direction
+            x = x_start
+            y += size
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
 from chessboard import chessboard
 if __name__ == "__main__":
      c1=chessboard(20,800,600)
+     c1.drawBoard("salam")
